d815fd85a901f58cc6cb0665bcf86617db6ad74f
Henry Wu <hywu925@gmail.com>
2019-08-06 22:23:14 -0700

    feat: Add barebones game page
    Renamed App files to Game, updated imports for new files.
    Added original CSS, but updated the background image import

---
+++ src/Game.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Game.css';
+
+const Game: React.FC = () => {
+  return (
+    <div className="container">
+      <header>
+        <h1>Matching Game</h1>
+      </header>
+    </div>
+  );
+}
+
+export default Game;
