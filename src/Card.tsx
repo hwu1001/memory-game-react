40f72658574a69f6467939c774c659d080690667
Henry Wu <hywu925@gmail.com>
2019-08-08 22:59:45 -0700

    feat: Implement card reveal on click
    Changed cards state object so that class names can be determined
    by Game component. Added more props to Card to handle class
    updates and callback for onClick event. Right now clicking a
    card will reveal it and start the timer if it's the first card.
    Need to look into hooks for these sorts of updates more to see
    if it's more intuitive to use a hook instead of simply calling
    a setState function.

--- src/Card.tsx
+++ src/Card.tsx
@@ -1,13 +1,23 @@
 import React from 'react';
 
 interface CardProps {
-  icon: string
+  icon: string,
+  index: number,
+  cardClickCb: (index: number) => void,
+  cardClassNames?: string[]
 }
 
 const Card: React.FC<CardProps> = (props) => {
   const cls = 'fa ' + props.icon;
+  let cardCls: string;
+  if (props.cardClassNames && props.cardClassNames.length > 0) {
+    cardCls = props.cardClassNames.join(' ') + ' card';
+  } else {
+    cardCls = 'card';
+  }
+  
   return (
-    <li className="card">
+    <li className={cardCls} onClick={() => props.cardClickCb(props.index)}>
         <i className={cls}></i>
     </li>
   );
--- src/Game.tsx
+++ src/Game.tsx
@@ -1,29 +1,133 @@
 import React, { useState, useEffect } from 'react';
 import './Game.css';
 import ScorePanel from './ScorePanel';
-import Deck from './Deck';
+import Card from './Card';
+
+interface ICard {
+  icon: string,
+  classNames: string[]
+}
 
 const Game: React.FC = () => {
-  let [cards, setCards] = useState([
-    'fa-diamond',
-    'fa-paper-plane-o',
-    'fa-anchor',
-    'fa-bolt',
-    'fa-cube',
-    'fa-anchor',
-    'fa-leaf',
-    'fa-bicycle',
-    'fa-diamond',
-    'fa-bomb',
-    'fa-leaf',
-    'fa-bomb',
-    'fa-bolt',
-    'fa-bicycle',
-    'fa-paper-plane-o',
-    'fa-cube'
+  let [cards, setCards] = useState<ICard[]>([
+    {
+      icon: 'fa-diamond',
+      classNames: []
+    },
+    {
+      icon: 'fa-paper-plane-o',
+      classNames: []
+    },
+    {
+      icon: 'fa-anchor',
+      classNames: []
+    },
+    {
+      icon: 'fa-bolt',
+      classNames: []
+    },
+    {
+      icon: 'fa-cube',
+      classNames: []
+    },
+    {
+      icon: 'fa-anchor',
+      classNames: []
+    },
+    {
+      icon: 'fa-leaf',
+      classNames: []
+    },
+    {
+      icon: 'fa-bicycle',
+      classNames: []
+    },
+    {
+      icon: 'fa-diamond',
+      classNames: []
+    },
+    {
+      icon: 'fa-bomb',
+      classNames: []
+    },
+    {
+      icon: 'fa-leaf',
+      classNames: []
+    },
+    {
+      icon: 'fa-bomb',
+      classNames: []
+    },
+    {
+      icon: 'fa-bolt',
+      classNames: []
+    },
+    {
+      icon: 'fa-bicycle',
+      classNames: []
+    },
+    {
+      icon: 'fa-paper-plane-o',
+      classNames: []
+    },
+    {
+      icon: 'fa-cube',
+      classNames: []
+    }
   ]);
   let [seconds, setSeconds] = useState(0);
   let [isTimerActive, setIsTimerActive] = useState(false);
+  // let [openCardIndex, setOpenCardIndex] = useState<number>(-1);
+
+  const handeCardClick = (index: number) => {
+    if (cards[index].classNames.includes('open')) {
+      return;
+    }
+
+    // Start the game timer if this is the first action
+    if (!isTimerActive) {
+      toggleTimer();
+    }
+    let cardsCopy = cards.slice(0, cards.length);
+    let clickedCardClsNames = cards[index].classNames.slice(0, cards[index].classNames.length);
+
+    toggleShowCardClasses(clickedCardClsNames);
+    cardsCopy[index].classNames = clickedCardClsNames;
+    setCards(cardsCopy);
+    // Either the player has an active card open already or doesn't
+    // if (openCardIndex > -1) {
+    //   toggleShowCardClasses(clickedCardClsNames);
+    // } else {
+    //   toggleShowCardClasses(clickedCardClsNames);
+    // }
+  };
+
+  // const addMatchedPair = (firstCardCls: string[], secondCardCls: string[]) => {
+  //   toggleShowCardClasses(firstCardCls);
+  //   toggleMatchBounce(firstCardCls);
+  //   toggleMatchBounce(secondCardCls);
+  // }
+
+  // const toggleMatchBounce = (arr: string[]) => {
+  //   for (const iterator of ['match', 'animated', 'bounce']) {
+  //     toggleClass(arr, iterator);
+  //   }
+  // }
+
+  const toggleShowCardClasses = (arr: string[]) => {
+    for (const iterator of ['open', 'show', 'animated', 'flipInY']) {
+      toggleClass(arr, iterator);
+    }
+  };
+
+  const toggleClass = (arr: string[], cls: string) => {
+    let i = arr.indexOf(cls);
+    if (i > -1) {
+      arr.splice(i, 1);
+    } else {
+      arr.push(cls);
+    }
+  };
 
   const toggleTimer = () => {
     setIsTimerActive(!isTimerActive);
@@ -47,7 +151,6 @@ const Game: React.FC = () => {
     let interval: number | undefined;
     const t: TimerHandler = () => {
       setSeconds(seconds => seconds + 1);
-      console.log(seconds);
     };
 
     if (isTimerActive) {
@@ -59,10 +162,10 @@ const Game: React.FC = () => {
   }, [isTimerActive, seconds]);
 
   useEffect(() => {
-    const shuffle = (arr: Array<string>) => {
+    const shuffle = (arr: ICard[]) => {
       let retArr = arr.slice(0, arr.length);
       let curIndex = retArr.length;
-      let tempValue: string;
+      let tempValue: ICard;
       let randomIndex: number;
       while (curIndex !== 0) {
         randomIndex = Math.floor(Math.random() * curIndex);
@@ -73,10 +176,10 @@ const Game: React.FC = () => {
       }
       return retArr;
     };
-    setCards(c => shuffle(c)); // runs only once, not on every re-render
-  }, []);
+    setCards(c => shuffle(c));
+  }, []); // runs only once, not on every re-render
 
-  
+
   return (
     <div className="container">
       <header>
@@ -87,7 +190,18 @@ const Game: React.FC = () => {
         secondsDisplay={padTimeString(Math.floor(seconds % 60))}
         resetCb={reset}
       />
-      <Deck cards={cards} />
+      <ul className="deck">
+        {cards.map((card, index) => {
+        return (
+          <Card
+            key={index}
+            icon={card.icon}
+            index={index}
+            cardClickCb={handeCardClick}
+            cardClassNames={card.classNames} 
+          />);
+        })}
+      </ul>
     </div>
   );
 }
